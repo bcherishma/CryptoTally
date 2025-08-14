d0d6a18a1a85f4a1ca9f980879b2699f06b9afa4
bcherishma <bcherishma@gmail.com>
2025-08-14 22:50:22 +0530

    Voting file

---
+++ voting.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+interface IVerifier{
+    function verifyProof(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[3] memory input
+    ) external view returns(bool r);
+}
